1908fcccce37404807913c6792d4da6a4c055b8f
Rex A <rexcanuforo@yahoo.com>
2020-05-16 18:30:22 -0400

    finished burger_controller

--- controllers/burger_cotroller.js
+++ controllers/burger_cotroller.js
@@ -13,3 +13,33 @@ router.get("/", function(req, res) {
     res.render("index", hbsObject);
   });
 });
+
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger, req.body.devoured
+    ], function(result) {
+      
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        
+        return res.status(err).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  module.exports = router;
