3bf40972de4e345fd4fcaccbf45e6a716af270eb
Rex A <rexcanuforo@yahoo.com>
2020-05-16 15:50:52 -0400

    made connections

--- server.js
+++ server.js
@@ -2,6 +2,33 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const mysql = require("mysql");
+
+
+require('dotenv').config();
+
+const connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASSWORD,
+    database: "burgers_db"
+  });
+};
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 // PORT setup for the application
 const PORT = process.env.PORT || 3000;
